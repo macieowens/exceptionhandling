729c3ceaecb3be8809cf4e4ca14b951038cd25fd
macieowens <112588931+macieowens@users.noreply.github.com>
2022-12-14 17:13:13 -0600

    initial commit

---
+++ ExceptionHandlingExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExceptionHandlingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var arr = new char[] { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
+            var numbers = new List<int>();
+            var str = "";
+
+            
+            foreach (var item in arr)
+            {
+                try
+                {
+                    str = item.ToString();
+                    var number = int.Parse(str);
+
+                    numbers.Add(number);
+
+                }catch(Exception e)
+                {
+                    Console.WriteLine($"Unable to Parse '{item}'");
+                }
+            }
+            try
+            {
+                numbers.Sort();
+            }
+
+            finally
+            {
+                Console.WriteLine(numbers);
+            }
+            
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+                
+        }
+    }
+}
